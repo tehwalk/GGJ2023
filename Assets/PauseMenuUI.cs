4b57335cee1b92f55bd91d5e56617a62e005119e
virimine <virimine@gmail.com>
2023-02-04 21:14:55 +0200

    Pause Menu

---
+++ Assets/PauseMenuUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenuUI : MonoBehaviour {
+
+	[SerializeField] GameObject basePanel;
+	[SerializeField] Button continueButton;
+	[SerializeField] Button quitButton;
+
+	public bool isOpen { get; private set; }
+
+	void Start() {
+		basePanel.SetActive(isOpen);
+		continueButton.onClick.AddListener(TogglePanel);
+		quitButton.onClick.AddListener(RestartGame); 
+	}
+
+	void Update() {
+		if (Input.GetButtonDown("Cancel")) { TogglePanel(); }
+	}
+
+	void TogglePanel() {
+		isOpen = !isOpen;
+		basePanel.SetActive(isOpen);
+	}
+
+	void RestartGame() => SceneManager.LoadScene("MainMenuScene");
+}
